1aa2670a42ef9da0621269ec610be54d9a796f86
Ekaterina <seemenkina@gmail.com>
2020-03-22 19:16:42 +0700

    add stack

---
+++ stack.go
@@ -0,0 +1,41 @@
+package lrparser
+
+type item struct {
+	value interface{} // value as interface type to hold any data type
+	next  *item
+}
+
+type Stack struct {
+	top  *item
+	size int
+}
+
+func (stack *Stack) IsEmpty() bool {
+	if stack.Len() == 0 {
+		return true
+	}
+	return false
+}
+
+func (stack *Stack) Len() int {
+	return stack.size
+}
+
+func (stack *Stack) Push(value interface{}) {
+	stack.top = &item{
+		value: value,
+		next:  stack.top,
+	}
+	stack.size++
+}
+
+func (stack *Stack) Pop() (value interface{}) {
+	if stack.Len() > 0 {
+		value = stack.top.value
+		stack.top = stack.top.next
+		stack.size--
+		return
+	}
+
+	return nil
+}
